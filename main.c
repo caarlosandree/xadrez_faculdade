67794ba59a84c9dacfe1e7d682d5df48805a6ded
Carlos André Sabino <caarlosandree@gmail.com>
2025-05-21 09:20:16 -0300

    nivel_aventureiro.c
    feat: Implementa movimentação do Cavalo com estrutura de loops aninhados
    
    - Adiciona lógica de movimentação em "L" do Cavalo (2 casas para baixo e 1 para esquerda)
    - Utiliza loop `for` para os movimentos verticais (↓) e um `for` aninhado para o movimento horizontal (←)
    - Mantém estrutura clara e comentários explicativos

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
     Autor: Carlos André Sabino
-    Nível: Novato
+    Nível: Aventureiro
     Descrição:
     Este programa simula a movimentação das peças Bispo, Torre e Rainha,
     utilizando estruturas de repetição e boas práticas de codificação.
@@ -12,17 +12,24 @@
 int mov_bispo=5;
 int mov_torre=5;
 int mov_rainha=8;
+int mov_cavalo = 1;
+int mov_cavalo2 = 2;
 
 int main() {
     int i; //Controle de Laços
+    int j; //Controle de Laços
+
+    printf("==================================\n");
+    printf(" Movimentação das peças de xadrez\n");
+    printf("==================================\n");
 
     printf("Movimentação do Bispo\n");
     // O Bispo se move 5 casas na diagonal superior direita,
     // representada por uma combinação de "Cima" e "Direita".
 
     for (i=0; i < mov_bispo; i++) {
-        printf("cima ^\n");
-        printf("direita ->\n");
+        printf("↑ cima\n");
+        printf("→ direita\n");
     }
 
     printf("\n");
@@ -32,7 +39,7 @@ int main() {
 
     i=0; //Reinicia o contador.
     while (i < mov_torre) {
-        printf("direita ->\n");
+        printf("→ direita\n");
         i++;
     }
 
@@ -43,11 +50,31 @@ int main() {
 
     i=0; //Reinicia o contador.
     do {
-        printf("<- esquerda\n");
+        printf("← esquerda\n");
         i++;
     } while (i < mov_rainha);
 
+    printf("\n");
 
+    printf("Movimentação do Cavalo\n");
+    // O Cavalo se move 2 baixo e 1 esquerda.
+    // O movimento do cavalo é representado por dois movimentos:,
+    // representada por um 'L'.
+
+
+    // Primeiro movimento: 2 casas para baixo
+    for (i = 0; i < mov_cavalo2; i++) {
+        printf("baixo ↓\n");
+
+        // Depois do segundo "baixo", faz a esquerda
+        if (i == mov_cavalo2 - 1) {
+            for (j = 0; j < mov_cavalo; j++) {
+                printf("esquerda ←\n");
+            }
+        }
+    }
+
+    printf("\n");
     printf("Fim do programa.");
 
     return 0;
